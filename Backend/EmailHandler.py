2e9d6179d54e93a34c6d05e79598e681adb37469
Mohamed Al-Ashmawy <mohamed.25250@eng.modern-academy.edu.eg>
2021-11-17 13:52:36 +0200

    Wrapped the code: SendReport(), ReadFormResponse()

--- Backend/EmailHandler.py
+++ Backend/EmailHandler.py
@@ -1,31 +1,34 @@
 import smtplib
 import pandas as pd
 from email.mime.text import MIMEText
+import sys
 
-# connect with Google's servers
-smtp_ssl_host = 'smtp.gmail.com'
-smtp_ssl_port = 465
+def SendReport():
 
-# use username or email to log in
-username = 'username@gmail.com'
-password = 'password'
+    # connect with Google's servers
+    smtp_ssl_host = 'smtp.gmail.com'
+    smtp_ssl_port = 465
 
-from_addr = 'username@gmail.com'
-to_addrs = ['detsname@gmail.com']
+    # use username or email to log in
+    username = 'username@gmail.com'
+    password = 'password'
 
-# the email lib has a lot of templates
-# for different message formats,
-# on our case we will use MIMEText
-# to send only text
-message = MIMEText('This Message is sent to you from Mohamed Al-Ashmawy as a first attempt to automate sending/receiving e-mails :)')
-message['subject'] = 'Mail Test'
-message['from'] = from_addr
-message['to'] = ', '.join(to_addrs)
+    from_addr = 'username@gmail.com'
+    to_addrs = ['detsname@gmail.com']
 
-# we'll connect using SSL
-server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
-# to interact with the server, first we log in
-# and then we send the message
-server.login(username, password)
-server.sendmail(from_addr, to_addrs, message.as_string())
-server.quit()
+    # the email lib has a lot of templates
+    # for different message formats,
+    # on our case we will use MIMEText
+    # to send only text
+    message = MIMEText('This Message is sent to you from The QC System :)' + sys.date())
+    message['subject'] = 'Report for CourseName' # CourseName is from the recieved e-mail
+    message['from'] = from_addr
+    message['to'] = ', '.join(to_addrs)
+
+    # we'll connect using SSL
+    server = smtplib.SMTP_SSL(smtp_ssl_host, smtp_ssl_port)
+    # to interact with the server, first we log in
+    server.login(username, password)
+    # and then we send the message
+    server.sendmail(from_addr, to_addrs, message.as_string())
+    server.quit()
--- Backend/GoogleFormsAnalysis.py
+++ Backend/GoogleFormsAnalysis.py
@@ -2,21 +2,23 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import pandas as pd
 
-# Initializing a GoogleAuth Object
-gauth = GoogleAuth()
+def ReadFormResponse():
+	
+	# Initializing a GoogleAuth Object
+	gauth = GoogleAuth()
 
-# client_secrets.json file is verified
-# and it automatically handles authentication
-gauth.LocalWebserverAuth()
+	# client_secrets.json file is verified
+	# and it automatically handles authentication
+	gauth.LocalWebserverAuth()
 
-# GoogleDrive Instance is created using
-# authenticated GoogleAuth instance
-drive = GoogleDrive(gauth)
+	# GoogleDrive Instance is created using
+	# authenticated GoogleAuth instance
+	drive = GoogleDrive(gauth)
 
-# Initialize GoogleDriveFile instance with file id
-file_obj = drive.CreateFile({'id': 'FILE_ID'})
-file_obj.GetContentFile('FILE_NAME.xls',
-		mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+	# Initialize GoogleDriveFile instance with file id
+	file_obj = drive.CreateFile({'id': 'FILE_ID'})
+	file_obj.GetContentFile('FILE_NAME.xls',
+			mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
-dataframe = pd.read_excel('FILE_NAME.xls')
-print(dataframe.head(10))
+	dataframe = pd.read_excel('FILE_NAME.xls')
+	print(dataframe.head(10))
