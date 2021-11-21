2f4bba5804e567bdb4e1620d24a76067ffa4dac6
Mohamed Al-Ashmawy <mohamed.25250@eng.modern-academy.edu.eg>
2021-11-21 17:54:28 +0200

    Finished read_form_response :)

--- Backend/Google Forms Handler/GoogleFormsHandler.py
+++ Backend/Google Forms Handler/GoogleFormsHandler.py
@@ -1,24 +1,45 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import pandas as pd
+import os
 
-def read_form_response():
-	
+def drive_access():
 	# Initializing a GoogleAuth Object
 	gauth = GoogleAuth()
 
 	# client_secrets.json file is verified
 	# and it automatically handles authentication
-	gauth.LocalWebserverAuth()
+	gauth.LoadCredentialsFile("creds.txt")
+	
+	if gauth.credentials is None:
+    	# Authenticate if they're not there
+		GoogleAuth.DEFAULT_SETTINGS['client_config_file'] = os.getcwd() + '\Backend\Google Forms Handler\client_secrets.json'
+		gauth.LocalWebserverAuth()
+	elif gauth.access_token_expired:
+    	# Refresh them if expired
+		gauth.Refresh()
+	else:
+    	# Initialize the saved creds
+		gauth.Authorize()
+	
+	gauth.SaveCredentialsFile("creds.txt")
 
 	# GoogleDrive Instance is created using
 	# authenticated GoogleAuth instance
 	drive = GoogleDrive(gauth)
+	return drive
 
+
+def read_form_response():
+	
+	
+	drive = drive_access()
 	# Initialize GoogleDriveFile instance with file id
-	file_obj = drive.CreateFile({'id': 'FILE_ID'})
-	file_obj.GetContentFile('FILE_NAME.xls',
+	file_obj = drive.CreateFile({'id': '154N9rY_gy8uK6unUbFi_VGQnDnFfEatsiqjZslCftyo'})
+	file_obj.GetContentFile('Test.xls',
 			mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
-	Form_Excel = pd.read_excel('FILE_NAME.xls')
+	Form_Excel = pd.read_excel('Test.xls')
 	return Form_Excel
+
+print(read_form_response())
