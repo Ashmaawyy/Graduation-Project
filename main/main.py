d64a575911e361bc984b9b4dfd98f60b21cfcc81
Ashmaawyy <al_ashmawy@outlook.com>
2022-02-03 14:32:36 +0200

    Renamed Data Pipelines to resolve import error

--- main/main.py
+++ main/main.py
@@ -1,2 +1,8 @@
-from EmailHandler import *
-from GoogleFormsHandler import *
+# pylint: disable=missing-module-docstring
+# pylint: disable=unused-wildcard-import
+#pylint: disable=wildcard-import
+from Emails.EmailHandler import *
+from GoogleForms.GoogleFormsHandler import *
+
+send_email("Prof Dr.Seham, We need some typical e-mails you'd send to the system :)",
+['seham.moawad@eng.modern-academy.edu.eg'])
