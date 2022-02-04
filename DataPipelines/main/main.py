0a33a1066ae11fc3cf87af2c3f77bb5201a86d3d
Ashmaawyy <al_ashmawy@outlook.com>
2022-02-04 13:27:37 +0200

    renamed files to confront to snake_case naming

--- DataPipelines/main/main.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=unused-wildcard-import
-#pylint: disable=wildcard-import
-from Emails.EmailHandler import *
-from GoogleForms.GoogleFormsHandler import *
-
-send_email("Prof Dr.Seham, We need some typical e-mails you'd send to the system :)",
-['mohamed204798@gmail.com'])
---
+++ data_pipelines/main/main.py
@@ -0,0 +1,8 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=unused-wildcard-import
+#pylint: disable=wildcard-import
+from emails.email_handler import *
+from google_forms.google_forms_handler import *
+
+#send_email("Prof Dr.Seham, We need some typical e-mails you'd send to the system :)",
+#['mohamed204798@gmail.com'])
