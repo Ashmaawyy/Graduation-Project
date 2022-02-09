795e8d9d7ac727389c9f7d67e30b4209d4d54532
Ashmaawyy <al_ashmawy@outlook.com>
2022-02-09 07:09:14 +0200

    appending sys.path seems to solve import issue

--- data_pipelines/main/main.py
+++ data_pipelines/main/main.py
@@ -1,6 +1,9 @@
 # pylint: disable=missing-module-docstring
 # pylint: disable=unused-wildcard-import
 #pylint: disable=wildcard-import
+import sys
+sys.path.append('./data_pipelines')
+
 from emails.email_handler import *
 from google_forms.google_forms_handler import *
 
