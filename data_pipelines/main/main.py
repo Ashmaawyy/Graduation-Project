3e76778cdf93c85a72b002b8124a70775d4ca0d8
Ashmaawyy <al_ashmawy@outlook.com>
2022-02-09 07:22:31 +0200

    added comment on the hacky fix :)

--- data_pipelines/main/main.py
+++ data_pipelines/main/main.py
@@ -2,10 +2,7 @@
 # pylint: disable=unused-wildcard-import
 #pylint: disable=wildcard-import
 import sys
-sys.path.append('./data_pipelines')
+sys.path.append('./data_pipelines') # Hacky fix to import issue :)
 
 from emails.email_handler import *
 from google_forms.google_forms_handler import *
-
-#send_email("Prof Dr.Seham, We need some typical e-mails you'd send to the system :)",
-#['mohamed204798@gmail.com'])
