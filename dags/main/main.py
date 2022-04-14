3b6ae15167ca0befc5e760e79051c7d2e2cf7b3f
Ashmaawyy <al_ashmawy@outlook.com>
2022-04-14 02:50:58 +0200

    edited the hacky fix to match new naming

--- dags/main/main.py
+++ dags/main/main.py
@@ -2,7 +2,7 @@
 # pylint: disable=unused-wildcard-import
 #pylint: disable=wildcard-import
 import sys
-sys.path.append('./data_pipelines') # Hacky fix to import issue :)
+sys.path.append('./dags') # Hacky fix to import issue :)
 
 from emails.email_handler import *
 from google_forms.google_forms_handler import *
