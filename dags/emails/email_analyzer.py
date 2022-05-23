2329f4d223058498ceee32fea37dd34b697053de
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-23 22:13:32 +0200

    Just add an extra space for visual appeal : )

--- dags/emails/email_analyzer.py
+++ dags/emails/email_analyzer.py
@@ -8,19 +8,24 @@ from html.parser import HTMLParser
 messages_df = recieve_emails_into_df()
 body_data = []
 body_html_comments = []
+
+
 class Parser(HTMLParser):
-    '''
+    """
     A class that parses HTML junk :)
-    '''
+    """
+
     # method to append the data between the tags to the list all_data.
     def handle_data(self, data):
         body_data.append(data)
+
     # method to append the comment to the list comments.
     def handle_comments(self, data):
         body_html_comments.append(data)
 
+
 # Creating an instance of our class.
 parser = Parser()
-# Poviding the input.
+# Providing the input.
 for i in range(len(messages_df['body'])):
     parser.feed(str(messages_df['body'][i]))
