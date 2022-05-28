a617c961db106d9cf12798c321715a8610d5bfbe
Ashmaawyy <al_ashmawy@outlook.com>
2022-05-28 16:22:45 +0200

    fixed a typo in the dag : )

--- dags/emails/emails_dag.py
+++ dags/emails/emails_dag.py
@@ -12,7 +12,7 @@ message_text = '''This Message is sent to you by the QC Department
 with DAG(dag_id="emails_dag",
          start_date = datetime(2023,7,1),
          schedule_interval = "@yearly",
-         catchup=False) as dag:
+         catchup = False) as dag:
 
          task1 = PythonOperator(
              task_id = "send_subbmission_email_to_doctors",
